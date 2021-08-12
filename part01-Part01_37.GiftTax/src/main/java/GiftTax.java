fddb8ecff6a669768cda4556401aa0fa555d1577
josiah34 <josiah_g@live.com>
2021-08-12 01:26:13 -0400

    Part 1-37 Gift Tax
    Passed and completed

--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -5,6 +5,22 @@ public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("Value of the gift?");
+        double gift = Integer.valueOf(scan.nextLine());
+
+        if (gift >= 5000 && gift < 25000) {
+            System.out.println((gift - 5000) * 0.08 + 100);
+        } else if (gift < 5000) {
+            System.out.println("No tax!");
+        } else if (gift >= 25000 && gift < 55000) {
+            System.out.println((gift - 25000) * 0.10 + 1700);
+        } else if (gift >= 55000 && gift < 200000) {
+            System.out.println((gift - 55000) * 0.12 + 4700);
+        } else if (gift >= 200000 && gift < 1000000) {
+            System.out.println((gift - 200000) * 0.15 + 22100);
+        }  else if (gift >= 1000000) {
+            System.out.println((gift - 1000000) * 0.17 + 142100);
+        }
+         
     }
 }
