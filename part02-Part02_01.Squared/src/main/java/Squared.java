7fafe8fd61307472e6f250cef0309e413842bc1e
josiah34 <josiah_g@live.com>
2021-08-12 01:40:03 -0400

    Part 2- Part 1 Squared
    Passed

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,10 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        int number = Integer.parseInt(scanner.nextLine());
+        int square = number * number;
+        System.out.println(square);
 
     }
 }
