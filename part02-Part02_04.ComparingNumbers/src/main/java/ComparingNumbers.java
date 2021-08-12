20969d63a233451dc7c400fb9f01bb1f23c82b8a
josiah34 <josiah_g@live.com>
2021-08-12 11:25:58 -0400

    Part 2-04 Comparing Numbers
    passed

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -5,6 +5,17 @@ public class ComparingNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter first number:");
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter second number");
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+        
+        if (firstNumber > secondNumber) {
+            System.out.println(firstNumber+" is greater than "+secondNumber);
+        } else if (secondNumber > firstNumber) {
+            System.out.println(firstNumber+" is smaller than "+secondNumber);
+        } else{
+            System.out.println(firstNumber+" is equal to "+secondNumber);
+        }
     }
 }
