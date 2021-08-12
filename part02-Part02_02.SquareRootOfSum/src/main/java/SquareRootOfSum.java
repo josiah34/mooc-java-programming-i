f9ca869c62998ab4c070f91c0766ea00157c70e2
josiah34 <josiah_g@live.com>
2021-08-12 01:57:56 -0400

    part 2 square root
    passed

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,13 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("");
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.println("");
+        int number2 = Integer.parseInt(scanner.nextLine());
+        int sum = number + number2;
+        double squareroot = Math.sqrt(sum);
+        System.out.println(squareroot);
 
     }
 }
