5c6b3ee24afd7813574018d6d3515a598dc2b60a
josiah34 <josiah_g@live.com>
2021-08-15 10:54:28 -0400

    finished part 2 part 2
    while loops

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double numberCount = 0;
+        double sum = 0;
 
+        while (true) {
+            System.out.println("");
+
+            double number = Double.valueOf(scanner.nextLine());
+
+            if (number > 0) {
+                numberCount++;
+                sum = number + sum;
+                continue;
+            }
+            if (number == 0) {
+                break;
+            }
+        }
+        if (sum > 0) {
+            System.out.println("Average of the numbers: " + sum / numberCount);
+        } else if (sum <= 0) {
+            System.out.println("Cannot calculate the average");
+        }
     }
 }
