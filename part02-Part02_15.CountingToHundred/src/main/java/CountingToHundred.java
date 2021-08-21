f3e8ae9659e487818847920e3b98c6d91a23bafe
josiah34 <josiah_g@live.com>
2021-08-21 02:12:48 -0400

    new commit
    8/21/21

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,10 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("");
+        int number = Integer.valueOf(scanner.nextInt());
+        for (int i = number; i < 100 + 1; i++) {
+            System.out.println(i);
+        }
     }
 }
